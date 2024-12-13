1ae049201eb2e5787282152b7553f5b7a66fb711
jatinupadhayay <jatinupadhayay1204@gmail.com>
2024-12-13 09:32:12 +0530

    fourth commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,14 @@
-const pg = require("pg");
-const { Client } = pg;
-const client = new Client({
-  user: "postgres",
-  password: "Jatin@465697",
-  host: "localhost",
-  port: 5432,
-  database: "street eats",
-});
-client.connect();
+const { Client } = require('pg');
 
-module.exports = client;
+const connectionString = 'postgresql://pickout_owner:L5hS0JvwkoEM@ep-spring-resonance-a5o9b60f.us-east-2.aws.neon.tech/pickout?sslmode=require';
+
+const client = new Client({
+  connectionString: connectionString,
+});
+
+client.connect()
+  .then(() => console.log('Connected successfully'))
+  .then(() => client.query('SELECT version();'))
+  .then(res => console.log(res.rows[0]))
+  .catch(e => console.error(e))
+  .finally(() => client.end());
